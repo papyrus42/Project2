8c7de33f4d291155b6b22fbb2ecbea0f5e9c370d
Anna Scarborough <alscarbo@ksu.edu>
2020-02-07 11:26:24 -0600

    Created main platform class, plan on using inheritance for moving platforms

--- MonoGameWindowsStarter/Platform.cs
+++ MonoGameWindowsStarter/Platform.cs
@@ -23,19 +23,27 @@ namespace MonoGameWindowsStarter
             this.game = game;
         }
 
+        public void Initialize(float width, float height, float x, float y)
+        {
+            bounds.Width = width;
+            bounds.Height = height;
+            bounds.X = x;
+            bounds.Y = y;
+        }
         public void LoadContent(ContentManager cm, string name)
         {
-
+            platform = cm.Load<Texture2D>(name);
         }
 
         public void Update(GameTime gameTime)
         {
-
+            //check for collisions
+            //moving platforms will be an inheritance of this class
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-
+            spriteBatch.Draw(platform, bounds, Color.White);
         }
 
     }
